92efedf02f40c8f4e873db5d7ecd8cb9ac0d6df9
Stamperton <35846593+Stamperton@users.noreply.github.com>
2021-03-18 15:19:01 +0000

    Half Interaction System

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    Camera cam;
+    float timer;
+
+    I_Interactable currentInteractable = null;
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 0.2f)
+        {
+            timer = 0;
+            InteractionRaycast();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentInteractable != null)
+            {
+                currentInteractable.OnInteract();
+            }
+        }
+    }
+
+    void InteractionRaycast()
+    {
+        RaycastHit _hit;
+        Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, 5f);
+
+        I_Interactable _interactable = _hit.collider.GetComponent<I_Interactable>();
+        if (_interactable != null)
+        {
+            currentInteractable = _interactable;
+            //CURSOR ON
+        }
+        else
+        {
+            _interactable = null;
+            //CURSOR OFF
+        }
+    }
+}
